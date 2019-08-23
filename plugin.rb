b221129e3f483ba3f0258c4995778080e87fe88b
romanrizzi <rizziromanalejandro@gmail.com>
2019-08-23 14:25:16 -0300

    FIX: send_email should be false

--- plugin.rb
+++ plugin.rb
@@ -76,7 +76,7 @@ class DiscordAuthenticator < ::Auth::OAuth2Authenticator
     data = auth[:extra_data]
     if !user.approved && data[:auto_approve]
       reviewable = ReviewableUser.find_by(target: user)
-      reviewable&.perform(Discourse.system_user, :approve_user, send_email: send_mail)
+      reviewable&.perform(Discourse.system_user, :approve_user, send_email: false)
     end
     if (avatar_url = data[:avatar_url]).present?
       retrieve_avatar(user, avatar_url)
